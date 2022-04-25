33992219551d863c25a97c616538ed0d58721713
AOgneva <as.ogneva@gmail.com>
2022-04-25 16:49:29 +0500

    added db, entity and table for service-type list

--- src/main/java/ru/ogneva/clubtab/domain/ServiceType.java
+++ src/main/java/ru/ogneva/clubtab/domain/ServiceType.java
@@ -1,17 +1,35 @@
 package ru.ogneva.clubtab.domain;
 
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name="service_type")
 public class ServiceType {
+    @Id
+    @Column(name="id")
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name="name")
+    @NotNull
     private String name;
+
+    @Column(name="tag")
+    @NotNull
     private String tag;
+
+    @Column(name="gist")
+    @NotNull
     private String gist;
 }
