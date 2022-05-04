d2cb8a16c02dfab3e6b32cade75fe5e63e2428dd
Анна Огнева <as.ogneva@gmail.com>
2022-05-04 18:43:09 +0500

    slot entity & table added

--- src/main/java/ru/ogneva/clubtab/domain/SlotEntity.java
+++ src/main/java/ru/ogneva/clubtab/domain/SlotEntity.java
@@ -1,9 +1,19 @@
 package ru.ogneva.clubtab.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.time.Instant;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
+@Table(name="slot")
 public class SlotEntity {
     @Id
     private Long id;
@@ -14,15 +24,16 @@ public class SlotEntity {
     @Column(name ="duration")
     private Long duration;
 
+    @JoinColumn(name="service_type_id")
     @ManyToOne(targetEntity = ServiceTypeEntity.class)
     private ServiceTypeEntity serviceType;
 
+    @JoinColumn(name="executor_id")
     @ManyToOne(targetEntity = PersonEntity.class)
     private PersonEntity executor;
 
+    @JoinColumn(name="state_id")
     @ManyToOne(targetEntity = StateTypeEntity.class)
     private StateTypeEntity state;
-
-
 
 }
